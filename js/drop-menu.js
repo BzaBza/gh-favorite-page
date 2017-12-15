f7261ca85dd99643d73f47c6a96805a09d6eb722
bzabza <akkdlyapereliva@mail.ru>
2017-12-15 22:49:13 +0200

    fix drop js

--- js/drop-menu.js
+++ js/drop-menu.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
     let count = 0;
     $('.nav-button').click(function () {
+        count++;
         console.log(count);
-        for (let i = 0; i = count; i++) {
-            if (i%2 === 0) {
-                let dropMenu = $('.drop-nav-wrap');
-                dropMenu.css('display', 'flex');
-            }
-            else {
-                let dropMenu = $('.drop-nav-wrap');
-                dropMenu.css('display', 'none');
-            }}
-    })});
+        if (count % 2 === 0) {
+            let dropMenu = $('.drop-nav-wrap');
+            dropMenu.css('display', 'none');
+        }
+        else {
+            let dropMenu = $('.drop-nav-wrap');
+            dropMenu.css('display', 'flex');
+        }
+    })
+});
